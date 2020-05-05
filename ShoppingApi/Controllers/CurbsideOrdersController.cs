c5cdce7da7e4d7bc8d9395fefce1399b40e1d6ed
lutzmichaelschmidt <61434839+lutzmichaelschmidt@users.noreply.github.com>
2020-05-05 15:57:30 -0400

    Day 2

---
+++ ShoppingApi/Controllers/CurbsideOrdersController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Controllers
+{
+    public class CurbsideOrdersController
+    {
+    }
+}
